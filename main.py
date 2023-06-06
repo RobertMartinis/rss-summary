ec19a9b3bf5206f846941c2080a1c92ff098ff07
Robert Martinis <robertmartinis@hotmail.com>
2023-06-06 16:18:53 +0200

    Multithread the fetching

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
-from constants import URLS
+from constants import NUM_THREADS, URLS
 from tools.ParseXML import ParseXML
 from Summarize import Summarize
+from multiprocessing.pool import ThreadPool
 
 # User agent to use when fetching the RSS-feed.
 headers = {
     'User-Agent': "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0"
 }
 
+def summarize_articles(urls):
+    name = urls[0]
+    rss_url = urls[1]
+    summarizer = Summarize(rss_url, headers, name)
+    summarizer.fetch()
+    summaries = summarizer.summaries
+    parser = ParseXML(summaries, f'./{name}_feed.xml')
+    parser.write()
+
 if __name__ == '__main__':
-    # TODO: Multithread this loop.
+    thread_data = []
     for website in URLS:
-        summarizer = Summarize(URLS[website], headers, website)
-        summarizer.fetch()
-        summaries = summarizer.summaries
-        parser = ParseXML(summaries, f'./{website}_feed.xml')
-        parser.write()
+        sublist = []
+        sublist.append(website)
+        sublist.append(URLS[website])
+        thread_data.append(sublist)
+    with ThreadPool(NUM_THREADS) as pool:
+        pool.map(summarize_articles, thread_data)
